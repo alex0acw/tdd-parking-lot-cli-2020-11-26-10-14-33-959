4671bdf7c6c6756ebc22828723aa98f17c1a99f8
AUAL <alex.au@oocl.com>
2020-11-27 15:34:02 +0800

    [ParkingBoy][Alex Au] test: parking boy return null when car park full

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,17 +1,27 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
     private final int capacity;
+    private final List<Car> carList;
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
+        carList = new ArrayList<>();
     }
 
     public ParkingLot() {
         this.capacity = 10;
+        carList = new ArrayList<>();
     }
 
     public Ticket park(Car car) {
-        return new Ticket();
+        if (capacity - carList.size() > 0) {
+            this.carList.add(car);
+            return new Ticket();
+        } else
+            return null;
     }
 }
