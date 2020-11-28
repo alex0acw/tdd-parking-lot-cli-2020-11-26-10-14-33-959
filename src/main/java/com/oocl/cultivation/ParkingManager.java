ae3f004cbdff79d88c71d0c4d358c0f04523c0ce
Alex Au <alex_acw@outlook.com>
2020-11-29 02:19:22 +0800

    [Story6][Alex Au] test: should_manager_park_and_fetch_with_specified_managed_parking_boy_park_when_being_asked_to_park_given_multiple_parking_boys

---
+++ src/main/java/com/oocl/cultivation/ParkingManager.java
@@ -0,0 +1,14 @@
+package com.oocl.cultivation;
+
+public class ParkingManager {
+    public void manage(ParkingBoy mockParkingBoy) {
+    }
+
+    public Ticket park(ParkingBoy mockParkingBoy, Car car) {
+        return null;
+    }
+
+    public Car getCarByTicket(ParkingBoy parkingBoy, Ticket ticket) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/ParkingManagerTest.java
@@ -0,0 +1,26 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ParkingManagerTest {
+
+    @Test
+    void should_manager_park_and_fetch_with_specified_managed_parking_boy_park_when_being_asked_to_park_given_multiple_parking_boys() {
+        //given
+        ParkingManager parkingManager = new ParkingManager();
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        parkingManager.manage(parkingBoy);
+        parkingManager.manage(new ParkingBoy(new ParkingLot()));
+        Car car = new Car();
+        //when
+        Ticket ticket = parkingManager.park(parkingBoy, car);
+        Car actual = parkingManager.getCarByTicket(parkingBoy, ticket);
+        //then
+        assertEquals(car, actual);
+    }
+}
