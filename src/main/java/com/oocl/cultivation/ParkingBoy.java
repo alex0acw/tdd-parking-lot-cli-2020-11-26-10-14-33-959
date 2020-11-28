67b30e1698ca5ce70508e19371a05f1b0781cd88
Alex Au <alex_acw@outlook.com>
2020-11-28 21:33:20 +0800

    [Story3][Alex Au] feat: parking boy parking cars to multiple parking lots

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,20 +1,42 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingBoy {
-    private final ParkingLot parkingLot;
+    private List<ParkingLot> parkingLotList;
 
     public ParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
+        this.parkingLotList = new ArrayList<>();
+        this.parkingLotList.add(parkingLot);
     }
 
-    public Ticket park(Car car) throws NotEnoughParkingSlotException {
-        return parkingLot.park(car);
+    public Ticket park(Car car) {
+        Ticket ticket = null;
+        for (ParkingLot parkingLot : this.parkingLotList) {
+            try {
+                ticket = parkingLot.park(car);
+                break;
+            } catch (NotEnoughParkingSlotException ignored) {
+            }
+        }
+        return ticket;
     }
 
-    public Car getCarByTicket(Ticket ticket) throws UnrecognizedParkingTicketException {
-        return parkingLot.getCarByTicket(ticket);
+    public Car getCarByTicket(Ticket ticket) {
+        Car car = null;
+        for (ParkingLot parkingLot : this.parkingLotList) {
+            try {
+                car = parkingLot.getCarByTicket(ticket);
+                break;
+            } catch (UnrecognizedParkingTicketException ignored) {
+            }
+        }
+        return car;
     }
 
-    public void addParkingLot(ParkingLot parkingLot2) {
+    public void addParkingLot(ParkingLot parkingLot) {
+        this.parkingLotList.add(parkingLot);
+
     }
 }
